64a84ed3257b427d10c2acb9ae1241ee98ce5550
Max S <creatingalong@gmail.com>
2020-02-07 01:11:25 -0600

    added a lot more commens, password result now displayed in resultbox vs an alert()

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
-
+//runs the genPassword function when the Generate button is clicked
 document.getElementById("submit").onclick = function() {genPassword()};
 
+//password generation function
 function genPassword() {
     var pass_length = prompt("Enter a password length below between 8 and 128.");
     var number_char = prompt("Use Numbers? Y or N?");
@@ -10,6 +11,7 @@ function genPassword() {
     var lower_char = prompt("Lowercase Characters? Y or N?");
 
 
+    //sets char variables depending on the users response
     if(pass_length < 8 || pass_length > 128){
             prompt("Please enter a valid length!");
     }  else {
@@ -24,7 +26,6 @@ function genPassword() {
 
         if(upper_char == "y" || upper_char == "Y"){
             var chars3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            console.log("ran upper char possitive");
         }
 
         if(lower_char == "y" || lower_char == "Y"){
@@ -49,15 +50,19 @@ function genPassword() {
 
     }
 
+    //combines chars to generates character list
     var result = '';
     var characters = ''.concat(chars1, chars2, chars3, chars4);
-    
-    console.log("past chars");
+
+    //counts how many characters are in the characters array
     var charactersLength = characters.length;
+
+    //adds one character at a time to results until desired length is reached
     for ( var i = 0; i < pass_length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
 
-    alert(result);
+    //sets resultbox to the generators result
+    document.getElementById("myresult").value = result;
 
 }
